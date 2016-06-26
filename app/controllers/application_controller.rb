643832c977dbb1bf7ef51e6e5d90f220ba9f77c8
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-06-26 13:21:11 -0400

    added views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,38 @@
 
 class ApplicationController < Sinatra::Base
-  set :views, "#{settings.root}/../views"
   
+  configure do
+    set :views,         "app/views"
+    set :public_folder, 'public'
+  end
+
   get '/' do 
-    @clients = Client.all
+    # @clients = Client.all
 
     erb :'clients/index'
+  end
+
+  helpers do
+    # def logged_in?
+    #   !!current_user
+    # end
+
+    # def current_user
+    #   @current_user ||= User.find_by(email: session[:email]) if session[:email]
+    # end
+
+    # def login(email, password)
+    #   user = User.find_by(:email => email)
+    #   if user && user.authenticate(password)
+    #     session[:email] = user.email
+    #   else
+    #     redirect '/login'
+    #   end
+    # end
+
+    # def logout!
+    #   session.clear
+    # end
+
   end
 end
